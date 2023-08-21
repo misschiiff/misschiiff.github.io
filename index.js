cdd3b51be738b3d6afedbc828117aa7f6e1cd1d7
misschiiff <misschiiff@gmail.com>
2023-08-21 19:17:34 +0100

    about page modification in progress

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-$(".toggle-bars").click(function() {
-    $(".nav").toggleClass("show");
+/*$("toggle-bars").click(function() {
+$("navMenu").toggleClass("show");
+}); */
+
+const navToggle = document.getElementById('navToggle');
+const navMenu = document.getElementById('navMenu');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
 });
