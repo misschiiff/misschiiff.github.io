8f7c51b8d30003bcc3ba9e038bc774f3061da146
misschiiff <misschiiff@gmail.com>
2023-10-12 16:52:28 +0100

    updates

--- sub.js
+++ sub.js
@@ -5,38 +5,39 @@ document.getElementById("subscribeButton").addEventListener("click", function() 
 document.getElementById("closePopup").addEventListener("click", function() {
     document.getElementById("subscribePopup").style.display = "none";
 });
-
-
 document.getElementById("subscribeForm").addEventListener("submit", function(event) {
     event.preventDefault();
+    const email = document.querySelector("#subscribeForm input[type='email']").value;
+    // You can now handle the email submission here and provide feedback to the user.
+    console.log("Email submitted:", email);
 
+    // Optionally, display a success message to the user here.
+}); // Add the following code to handle form submission and send email data
+document.getElementById("subscribeForm").addEventListener("submit", function(event) {
+    event.preventDefault();
     const email = document.getElementById("emailInput").value;
 
-    // Define the URL where you want to post the email data
-    const postURL = "https://t.me/+mv9Q6J6CL04NGZk";
-
-    // Create a data object with the email
-    const data = {
-        email: email
-    };
-
-    // Display a "Submitting..." message while the request is in progress
-    document.getElementById("subscribeResponse").innerHTML = "Submitting...";
-
-    fetch(postURL, {
+    // Send the email to your server
+    fetch('/post-email', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(data),
+            body: JSON.stringify({ email }),
         })
-        .then(() => {
-            // Handle the submission success
-            document.getElementById("subscribeResponse").innerHTML = "Subscription successful!";
+        .then(response => {
+            if (response.ok) {
+                console.log('Email sent successfully.');
+                // Optionally, display a success message to the user here.
+            } else {
+                console.error('Failed to send email.');
+                // Optionally, display an error message to the user here.
+            }
         })
         .catch(error => {
-            // Handle any errors or failed requests
-            document.getElementById("subscribeResponse").innerHTML = "Subscription failed. Please try again.";
-            console.error('An error occurred:', error);
+            console.error('Error:', error);
+            // Optionally, display an error message to the user here.
         });
 });
+
+// Other JavaScript code for your website (remaining code remains the same)
